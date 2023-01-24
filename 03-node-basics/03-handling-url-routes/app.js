a6ec76b08df78a11b7d9638b7b296d064fb2e6fe
Sreejith Sathyan <sreejithsathyans@gmail.com>
2023-01-24 10:40:59 +0530

    reading data from request

---
+++ 03-node-basics/03-handling-url-routes/app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require('fs');
+const { parse } = require("path");
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req; 
+  if (url === "/") {
+    res.write(`
+    <html lang="en">
+    <head>
+        <title>Form</title>
+    </head>
+    <body>
+        <form action="/messages" method='POST'>
+            <input type="text" name="messageBox">
+            <button type="submit">Send</button>
+        </form>
+    </body>
+    </html>
+    `);
+    return res.end();
+  }else if(url === '/messages' && method === 'POST'){
+    const body = [];
+    let parsedData = '';
+    let message = '';
+    req.on('data', (stream) =>{
+      body.push(stream);
+    })
+
+    req.on('end', ()=>{
+      parsedData = Buffer.concat(body).toString();
+      console.log(`Parsed Data = ${parsedData}`);
+      message = parsedData.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/')
+    return res.end();
+  }
+});
+
+server.listen(3000);
---
+++ 03-node-basics/03-handling-url-routes/readme.md
@@ -0,0 +1,4 @@
+## res.on() method
+* The on method binds an event to a object.
+* It is a way to express your intent if there is something happening (data sent or error in your case), then execute the function added as a parameter. This style of programming is called Event-driven programming. 
+* This example is just to demonstrate the row functioning of node.js. Express.js can help us to hide all these complexities with simple and easy to follow implementations.
